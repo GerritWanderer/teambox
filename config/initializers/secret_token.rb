0d7b18553ef837b78641909378bba406649c95f8
Gerrit Wanderer <wanderwelten@googlemail.com>
2011-06-18 12:02:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teambox::Application.config.secret_token = 'a9293be8f37c5c107100f5fcdbaa68758e1080cea011118573116c094e18764bcfd6874a65322b90555ed2eb69462245c53c2e5cda0793df97a5daad005bdbbe'
